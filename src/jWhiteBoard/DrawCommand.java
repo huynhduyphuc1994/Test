0110d7bdc94c53776e29717a8e019e31881d1d02
duythach248 <duythach248@gmail.com>
2014-12-12 12:00:42 +0700

    thay doi kich thuoc co

--- src/jWhiteBoard/DrawCommand.java
+++ src/jWhiteBoard/DrawCommand.java
@@ -18,6 +18,7 @@ public class DrawCommand implements Streamable {
     int x;
     int y;
     int rgb;
+	int brushSize;
     
     
     
@@ -35,6 +36,14 @@ public class DrawCommand implements Streamable {
         this.y=y;
         this.rgb=rgb;
     }
+    DrawCommand(byte mode, int x, int y, int rgb, int brushSize) {
+        this.mode=mode;
+        this.x=x;
+        this.y=y;
+        this.rgb=rgb;
+        this.brushSize=brushSize;
+        
+    }
 
 
     public void writeTo(DataOutput out) throws Exception {
@@ -42,6 +51,8 @@ public class DrawCommand implements Streamable {
         out.writeInt(x);
         out.writeInt(y);
         out.writeInt(rgb);
+        out.writeInt(brushSize);
+        
     }
 
     public void readFrom(DataInput in) throws Exception {
@@ -49,6 +60,7 @@ public class DrawCommand implements Streamable {
         x=in.readInt();
         y=in.readInt();
         rgb=in.readInt();
+        brushSize=in.readInt();
     }
 
 
